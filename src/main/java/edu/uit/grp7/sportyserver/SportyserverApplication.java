5f014960a3637b00ede983053e0283abec628582
tori4582 <hungtran.6360@gmail.com>
2022-05-06 00:19:53 +0700

    Init codebase for SportyServer

---
+++ src/main/java/edu/uit/grp7/sportyserver/SportyserverApplication.java
@@ -0,0 +1,13 @@
+package edu.uit.grp7.sportyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uit/grp7/sportyserver/SportyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uit.grp7.sportyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
